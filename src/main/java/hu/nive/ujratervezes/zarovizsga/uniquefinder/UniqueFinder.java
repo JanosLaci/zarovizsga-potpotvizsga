6eb8b3863d872790dede33cce81f64d665b24872
JanosLaci <janoslaszlo@gmail.com>
2021-04-27 12:13:50 +0200

    LinkedHashSet megoldás, majd listává:
    return new ArrayList<>(setOfChars);

--- src/main/java/hu/nive/ujratervezes/zarovizsga/uniquefinder/UniqueFinder.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/uniquefinder/UniqueFinder.java
@@ -5,27 +5,37 @@ package hu.nive.ujratervezes.zarovizsga.uniquefinder;
 //Minden karakter csak egyszer szerepeljen a listában, az inputban nézett első előfordulásának megfelelő helyen.
 //Ha a metódus null-t kap paraméterben, akkor dobj IllegalArgumentException-t!
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class UniqueFinder {
     public List<Character> uniqueChars(String inputString) {
 
         if (inputString == null) throw new IllegalArgumentException("No input received.");
 
-        List<Character> outputList = new ArrayList<>();
+
 
         //Tévút:
         //List<String> splitString = Arrays.asList(inputString.split(""));
         //List<Character> outputList = new ArrayList<Character>(Arrays.asList(inputString.toCharArray()));
 
+        //List megoldás:
+       /* List<Character> outputList = new ArrayList<>();
 
         for (char element :
                 inputString.toCharArray()) {
             if (!outputList.contains(element)) outputList.add(element);
+        }*/
+
+        Set<Character> setOfChars = new LinkedHashSet<>();
+
+        for (char element :
+                inputString.toCharArray()) {
+            setOfChars.add(element);
         }
 
-        return outputList;
+        //Nem lehet így, helyette  new ArrayList<>(result):
+        // List<Character> outputList = (List<Character>) setOfChars;
+
+        return new ArrayList<>(setOfChars);
     }
 }
