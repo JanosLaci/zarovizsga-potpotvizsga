b47dd205ae2b44e3cab95d73dbae9e40804d7397
JanosLaci <janoslaszlo@gmail.com>
2021-04-27 13:26:49 +0200

    DaVinciCode
    Files.newBufferedReader(Path.of(inputString))
    
    String line;
    while ((line=bufferedReader.readLine())!=null){}
    
    verzió

--- src/main/java/hu/nive/ujratervezes/zarovizsga/countchars/DaVinciCode.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/countchars/DaVinciCode.java
@@ -1,34 +1,42 @@
 package hu.nive.ujratervezes.zarovizsga.countchars;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DaVinciCode {
+
+    String[] acceptedInputs = "x10".split("");
+
     public int encode(String inputString, char inputChar) {
 
         // &&, nem ||
-        // konstans String, majd contains
-        if (inputChar!='x' && inputChar!='1' && inputChar!='0'){
+        // konstans Stringtömb splittel, majd contains Arrays.asList-tel
+        if (inputChar!='x' && inputChar!='1' && inputChar!='0')  {
             throw new IllegalArgumentException("Invalid input."); }
 
         int charCounter=0;
 
+
         try {
-            List<String> lines = Files.readAllLines(Path.of(inputString));
-            //int charCountInLine = 0;
-            for (String line :
-                    lines) {
+            BufferedReader bufferedReader = Files.newBufferedReader(Path.of(inputString));
+            String line;
+            while ((line=bufferedReader.readLine())!=null){
                 for (char element :
                         line.toCharArray()) {
                     //System.out.print(element);
                     if (element==inputChar) charCounter +=1;
                 }
+
             }
 
+
         } catch (IOException ioException) {
-            throw new IllegalArgumentException("Cannot red the file.", ioException);
+            throw new IllegalArgumentException("Cannot read the file.");
         }
 
 
